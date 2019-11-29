4d3442ebea852e3a5fc60042f4da7d742074541a
sjb <shangjiabin@163.com>
2019-11-29 21:00:02 +0800

    啦啦

--- app/src/main/java/com/bw/movie/view/activity/SheZhiActivity.java
+++ app/src/main/java/com/bw/movie/view/activity/SheZhiActivity.java
@@ -1,15 +1,34 @@
 package com.bw.movie.view.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import com.bw.movie.R;
-
 public class SheZhiActivity extends AppCompatActivity {
+
+    private ImageView iv;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_she_zhi);
+        iv = findViewById(R.id.iv);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btn = findViewById(R.id.btn_back);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SheZhiActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
